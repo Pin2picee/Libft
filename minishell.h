babf204f7e0265943a5654854eac01b03ba99eaa
Pin2picee <Pin2picee@users.noreply.github.com>
2024-11-11 16:33:47 +0100

    update 2.0

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: abelmoha <abelmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 09:41:44 by abelmoha          #+#    #+#             */
-/*   Updated: 2024/11/11 12:42:57 by abelmoha         ###   ########.fr       */
+/*   Updated: 2024/11/11 16:21:31 by abelmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,34 @@
 # include <unistd.h>
 # include <limits.h>
 
+typedef struct s_env
+{
+	char	*key;
+	char	*value;
+	struct s_var *next;
+}				t_env;
+
+
+typedef struct s_node 
+{
+	int	fd_in;
+	int	fd_out;
+	char	*entre_pipe;
+	char	*split_ep;
+	struct	s_node *next;
+}				t_node;
+
+// chaques noeuds est un entre pipe 
+typedef struct	s_minishell
+{
+	t_env	*var;
+	t_node	*start_node;
+}				t_minishell;
 /* FOR PROMPT*/
 char	*get_prompt(void);
+
+
+/*---PARSING---*/
+void	parsing(char	*line);
 
 #endif
--- parsing.c
+++ parsing.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abelmoha <abelmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 09:41:29 by abelmoha          #+#    #+#             */
-/*   Updated: 2024/11/11 16:03:07 by abelmoha         ###   ########.fr       */
+/*   Created: 2024/11/11 12:45:22 by abelmoha          #+#    #+#             */
+/*   Updated: 2024/11/11 12:46:03 by abelmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+void	parsing(char	*line)
 {
-	char	*line;
-	char	*prompt;
 	
-	(void)argv;
-	if (argc != 1)
-		return (perror("Error\n"), 1);
-	while (1)
-	{
-		prompt = get_prompt();
-		line = readline(prompt);
-		parsing()
-		free(prompt);
-	}
-    return 0;
 }
