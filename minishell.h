8769ad090d5e167a35f7e3621194272d6fe63d14
Pin2picee <Pin2picee@users.noreply.github.com>
2024-11-11 12:43:09 +0100

    update .h

--- minishell.h
+++ minishell.h
@@ -6,12 +6,12 @@
 /*   By: abelmoha <abelmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 09:41:44 by abelmoha          #+#    #+#             */
-/*   Updated: 2024/11/11 12:26:58 by abelmoha         ###   ########.fr       */
+/*   Updated: 2024/11/11 12:42:57 by abelmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHEL_H
+# define MINISHELL_H
 
 # include "libft/libft.h"
 # include "stdio.h"
