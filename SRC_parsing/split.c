bf58d141a003ba9983d517a048594526364bf90c
Pin2picee <Pin2picee@users.noreply.github.com>
2024-11-23 17:00:38 +0000

    update for split

--- SRC_parsing/split.c
+++ SRC_parsing/split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abelmoha <abelmoha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Pin2picee <Pin2picee@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:17:02 by abelmoha          #+#    #+#             */
-/*   Updated: 2024/11/22 20:48:35 by abelmoha         ###   ########.fr       */
+/*   Updated: 2024/11/23 00:19:06 by Pin2picee        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,45 @@
 
 // faire un split qui separs les mots des espaces et prend tout la quotes en entier
 
+int	count_word(char *command, int i, int count)
+{
+	while (command[i])
+	{
+		if (ft_strchr("\'\"", command[i]))
+		{
+			count++;
+			i += quote_chr(command, i);
+		}
+		if (command[i] == ' ' || command[i] == '\t')
+		{
+			while (command[i] == ' ' || command[i] == '\t')
+				i++;
+		}
+		if (command[i] != ' ' && command[i] == '\t')
+		{
+			while (command[i] != ' ' && command[i] != '\t')
+				i++;
+			if (command[i])
+				count++;
+		}
+		i++;
+	}
+	return (count_word);
+}
+
 char	**split_minishell(char *command, char sep)
 {
-	141585369+/**-+969++
-	
-	*/
+	char	**split;
+	int		i;
+
+	i = 0;
+	while (command[i])
+	{
+		if (ft_strchr("\'\"", command[i]))
+		{
+			*split = ft_strdup()
+		}
+	}
 }
 
 void	split_and_clean(t_node *node)
@@ -47,5 +81,4 @@ int	main(int argc, char **argv)
 	free(node->command);
 	free(node->hd);
 	free(node);
-	return (1);
 }
