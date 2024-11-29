6fdbcc3f28f69ccf26309e59eb643174e50a9534
Pin2picee <Pin2picee@users.noreply.github.com>
2024-11-29 16:51:39 +0100

    back up for reset hard

--- SRC_execution/ft_exit.c
+++ SRC_execution/ft_exit.c
@@ -1,13 +1,21 @@
 #include "../minishell.h"
-/*
 
-void    ft_exit()
+
+void    ft_exit(t_minishell *data)
 {
-    if (!tab[1])
+    if (!tab[1] && data->start_node->next == NULL)
     {
+        ft_free_all(data);
+        ft_printf("exit\n");
         exit(0);
     }
-    si il y a qu un seul arg, exit_code = 0;
+
+    exit(ft_atoi(av[1]));
+}
+
+
+
+ si il y a qu un seul arg, exit_code = 0;
     faire % 256 du nombre;
     gestion d erreurs:
     {
@@ -15,6 +23,3 @@ void    ft_exit()
         quand l argument n est pas un int; -> bash: exit: n: numeric argument required
     }
     printf("")
-    exit(ft_itoa(av[1]));
-}
-*/
